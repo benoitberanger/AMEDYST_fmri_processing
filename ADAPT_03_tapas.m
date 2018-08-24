d598cc4e93e01f233749164f1106d5f662c30c1a
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-08-24 18:45:24 +0200

    added noise ROI in in tapas

--- ADAPT_03_tapas.m
+++ ADAPT_03_tapas.m
@@ -8,7 +8,7 @@ physio_path = fullfile(pwd,'physio');
 dirFunc = e.getSerie('ADAPT_run\d$').toJob
 
 
-%%
+%% Fetch physio dirs
 
 for subj = 1 : length(dirFunc)
     
@@ -20,18 +20,23 @@ for subj = 1 : length(dirFunc)
         error('dir does not exists : %s', subj_physio)
     end
     
-    
-    
-    dirPhysio{subj}{1,1} = char(gdir(subj_physio,'ADAPT_run1_PhysioLog'));
-    dirPhysio{subj}{2,1} = char(gdir(subj_physio,'ADAPT_run2_PhysioLog'));
-    
-
+    dirPhysio{subj}{1} = char(gdir(subj_physio,'ADAPT_run1_PhysioLog'));
+    dirPhysio{subj}{2} = char(gdir(subj_physio,'ADAPT_run2_PhysioLog'));
+   
 end
 
 dirPhysio
 
 
+%% Fetch noise ROI dirs
+
+dirNoiseROI = e.getSerie('anat').toJob(0)
+
+
 %%
+
+par.file_reg = '^f.*nii'; % to fetch volume info (nrVolumes, nrSlices, TR, ...)
+
 
 par.run = 0;
 par.display = 1;
@@ -40,4 +45,4 @@ par.print_figures = 0;
 par.redo=1;
 
 par
-job_physio_tapas( dirFunc, dirPhysio, par);
+job_physio_tapas( dirFunc, dirPhysio, dirNoiseROI, par);
